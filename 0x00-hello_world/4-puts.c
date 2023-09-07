1ff4ceef1932de72b29dfd81a0e11d66420fe1c1
isaac-higzz <isaachigzz@gmail.com>
2023-09-07 05:57:56 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -C program thats prints alink with puts funtions
+ * return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
