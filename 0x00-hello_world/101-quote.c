6dc9f7a9d67af8b44d152640601fa7e6effcbd52
isaac-higzz <isaachigzz@gmail.com>
2023-09-09 03:46:30 -0700

    Write a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "abd that piece of atr is useful\"
+-Dora Korpar, 2015-10-19\n", 59);
+	return (1);
