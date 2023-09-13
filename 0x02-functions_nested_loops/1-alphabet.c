a0efaba45ff23c19fc6381a84a9b6539426a6540
isaac-higzz <isaachigzz@gmail.com>
2023-09-12 23:45:55 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main"
+
+/**
+ * print_alphabet -function to print alphabets in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar("\n");
+}
