559814bbeb538f5ed042deff9dbdba72e3f50bc8
isaac-higzz <isaachigzz@gmail.com>
2023-09-15 02:18:35 -0700

    5 more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,20 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int num, row, count;
 
-	for (i = 1; i <= 10; i++)
+	for (row = 1; row <= 10; row++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (count = 0; count <= 14; count++)
 		{
-			if (j >= 10)
-				_putcahr('1');
-			_putchar(j % 10 + '0');
+			num = count;
+			if (count > 9)
+			{
+				_putcahr(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
