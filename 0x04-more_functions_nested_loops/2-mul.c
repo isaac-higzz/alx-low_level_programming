43800c88fb92e0504e4f71975802626beb9b59d1
isaac-higzz <isaachigzz@gmail.com>
2023-09-15 01:35:49 -0700

    the multiplication of numbers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,13 @@
 
 /**
  * mul - multiplication of 2 int
- * @a: first int
- * @b: second int
+ * @a: first input
+ * @b: second input
  *
  * Return: addition of the multiplications
  */
 
-int mul(int a, int b);
+int mul(int a, int b)
 {
 	return (a * b);
 }
