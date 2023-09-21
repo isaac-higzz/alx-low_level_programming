3e20184691c483946437e78aff35a053c5523320
isaac-higzz <isaachigzz@gmail.com>
2023-09-21 15:48:35 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,7 +12,7 @@ char *leet(char *a);
 int _putchar(char c);
 char *rot13(char *a);
 void print_number(int n);
-cahr *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 
